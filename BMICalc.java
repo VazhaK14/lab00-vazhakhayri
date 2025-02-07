57431705b89db1e7d0761c4b659eee438696e183
Vazha Khayri <khayrivazhasocial@gmail.com>
2025-02-07 14:29:21 +0700

    fix: output

--- BMICalc.java
+++ BMICalc.java
@@ -13,12 +13,14 @@ public class BMICalc {
 
         double bmi = weight/(height*height);
         
-        if (bmi < 18.5) System.out.println("Underweight");
-        else if (bmi >= 18.5 && bmi <= 24.9) System.out.println("Normal");
-        else if (bmi >= 25.0 && bmi <= 29.0) System.out.println("Overweight");
-        else if (bmi >= 30.0 && bmi <= 40.0) System.out.println("Obese");
-        else if (bmi > 40) System.out.println("Obese");
-
+        if (bmi < 18.5) System.out.printf("Nilai BMI anda adalah Underweight \n Anda termasuk kategori %f \n", bmi);
+        else if (bmi >= 18.5 && bmi <= 24.9) System.out.printf("Nilai BMI anda adalah Normal \n Anda termasuk kategori %f \n", bmi);
+        else if (bmi >= 25.0 && bmi <= 29.0) System.out.printf("Nilai BMI anda adalah overweight \\n" + //
+                                                                " Anda termasuk kategori %d", bmi);
+        else if (bmi >= 30.0 && bmi <= 40.0) System.out.printf("Nilai BMI anda adalah Obese \\n" + //
+                                                                " Anda termasuk kategori %d", bmi);
+        else if (bmi > 40) System.out.printf("Nilai BMI anda adalah Extreme Obese \\n" + //
+                                                " Anda termasuk kategori %d", bmi);
         
     }
 }
